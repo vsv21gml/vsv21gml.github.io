d77abb73c7f69ec7d4b235d9b6c35e6553df8e41
heejun <vsv21gml@naver.com>
2023-02-14 21:31:18 +0900

    init

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+  outputDir: path.resolve(__dirname, "./wedding"),
+  css: {
+    loaderOptions: {
+      scss: {
+        additionalData: `@import "~@/assets/styles/_var.scss";`,
+      },
+    },
+  },
+  devServer: {
+    disableHostCheck: true,
+  },
+};
